18aca965ea8ad95475cb67f7ac4d9756cd5b8d68
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-06-18 14:37:02 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Wear_N_Code</title>
+        <meta name="description" content="Wear_N_Code - wear and code" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <div className='mx-4 bg-slate-500'>
+        hello world
+        </div>
+      </main>
+    </>
+  )
+}
