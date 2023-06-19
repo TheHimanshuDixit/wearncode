8a98471ff26917c9ba2cb01d88217ebfe4ef216a
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-06-19 19:27:00 +0530

    tshirts page created

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,16 +6,16 @@ import { AiOutlineShoppingCart } from 'react-icons/Ai';
 const Navbar = () => {
   return (
     <>
-      <div className='flex flex-col md:flex-row md:justify-start justify-center items-center p-2'>
-        <div className="logo mx-5">
-          <Image src="/logo.png" alt="logo" width={150} height={20} />
+      <div className='flex flex-col md:flex-row md:justify-start justify-center items-center p-2 shadow-xl'>
+        <div className="logo mx-10">
+          <Link href={"/"}><Image src="/logo.png" alt="logo" width={150} height={20} /></Link>
         </div>
-        <div className="nav">
-          <ul className="flex items-center space-x-2 font-bold">
-            <Link href={"/"}><li>Tshirts</li></Link>
-            <Link href={"/"}><li>Hoodies</li></Link>
-            <Link href={"/"}><li>Stickers</li></Link>
-            <Link href={"/"}><li>Mugs</li></Link>
+        <div className="nav pt-2 md:pt-0">
+          <ul className="flex items-center space-x-6 font-bold">
+            <Link href={"/tshirts"}><li>Tshirts</li></Link>
+            <Link href={"/hoodies"}><li>Hoodies</li></Link>
+            <Link href={"/stickers"}><li>Stickers</li></Link>
+            <Link href={"/mugs"}><li>Mugs</li></Link>
           </ul>
         </div>
         <div className="cart absolute right-0 mx-5  text-xl md:text-2xl "><AiOutlineShoppingCart /></div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Footer from '@/components/Footer'
-import Navbar from '@/components/Navbar'
 import Head from 'next/head'
 import Image from 'next/image'
 import Script from 'next/script'
@@ -12,32 +10,33 @@ export default function Home() {
         <meta name="description" content="Wear_N_Code - wear and code" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Navbar />
-      <div class="flex justify-center">
-        <div class="flex flex-col md:flex-row space-y-2  items-center justify-center max-w-5xl w-full px-4 md:px-10">
-          <div class="w-full md:w-1/2 mx-2 h-96 overflow-hidden  ">
-            <img src="https://source.unsplash.com/random/500x500/?clothes" alt="" class="h-full w-full" />
-          </div>
-          <div class="w-full md:w-1/2 mx-2 h-96 ">
-            <div class="flex flex-col space-y-2">
-              <div class="h-48 w-full overflow-hidden  ">
-                <img src="https://source.unsplash.com/random/300x200/?clothes" alt="" class="h-full w-full" />
-              </div>
-              <div class="h-48 w-full flex space-x-4 pb-3">
-                <div class="h-full w-1/2 overflow-hidden  ">
-                  <img src="https://source.unsplash.com/random/200x200/?clothes" alt="" class="h-full w-full" />
+      <main>
+        <div className="flex justify-center">
+          <div className="flex flex-col md:flex-row space-y-2  items-center justify-center max-w-5xl w-full px-4 md:px-10">
+            <div className="w-full md:w-1/2 mx-2 h-96 overflow-hidden  ">
+              <img src="https://source.unsplash.com/random/500x500/?clothes" alt="" className="h-full w-full" />
+            </div>
+            <div className="w-full md:w-1/2 mx-2 h-96 ">
+              <div className="flex flex-col space-y-2">
+                <div className="h-48 w-full overflow-hidden  ">
+                  <img src="https://source.unsplash.com/random/300x200/?clothes" alt="" className="h-full w-full" />
                 </div>
-                <div class="h-full w-1/2  overflow-hidden  ">
-                  <img src="https://source.unsplash.com/random/200x200/?clothes" alt="" class="h-full w-full" />
+                <div className="h-48 w-full flex space-x-4 pb-3">
+                  <div className="h-full w-1/2 overflow-hidden  ">
+                    <img src="https://source.unsplash.com/random/200x200/?clothes" alt="" className="h-full w-full" />
+                  </div>
+                  <div className="h-full w-1/2  overflow-hidden  ">
+                    <img src="https://source.unsplash.com/random/200x200/?clothes" alt="" className="h-full w-full" />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <Script src="https://cdn.tailwindcss.com"></Script>
+        <Script src="https://cdn.tailwindcss.com"></Script>
+      </main>
 
-      <Footer />
+
     </>
   )
 }
