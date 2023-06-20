96e2538a29639a7e6b77f294c2a3009f7dc5a0ba
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-06-21 04:32:13 +0530

    mongoose install and model created

---
+++ models/Order.js
@@ -0,0 +1,17 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+const OrderSchema = new mongoose.Schema({
+    userId: {type: String, required: true},
+    products: [
+        {
+            productId: {type: String, required: true},
+            quantity: {type: Number, default: 1}
+        }
+    ],
+    address: {type: String, required: true},
+    amount: {type: Number, required: true},
+    status: {type: String, default: 'Pending', required: true}
+}, {timestamps: true});
+
+export default mongoose.models.Order || mongoose.model("Order", OrderSchema);
