13c974a95998a91823175cd4d272b5201ed499bf
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-06-24 05:25:23 +0530

    orders page created

--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import router from 'next/router'
 
 const Profile = () => {
+
+    useEffect(() => {
+        if(!localStorage.getItem('token')) {
+            router.push('/login')
+        }
+    }, [])
+
+
     return (
         <div>Profile</div>
     )
