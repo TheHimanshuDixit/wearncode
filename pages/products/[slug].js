6621af94e6376903329a0ffc0a74b29399da08fd
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-07-08 17:00:22 +0530

    out of stock function is working

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -148,9 +148,10 @@ const Post = ({ buyNow, addToCart, product, variants }) => {
                                 </div>
                             </div>
                             <div className="flex">
-                                <span className="title-font font-medium text-2xl text-gray-900">₹58.00</span>
-                                <button onClick={() => { addToCart(slug, 1, product.price, product.title, product.size, product.color) }} className="flex ml-8 text-white bg-[#007fff] border-0 py-2 px-3 md:px-6 focus:outline-none hover:bg-indigo-600 rounded">Add to Cart</button>
-                                <button onClick={() => { buyNow(slug, 1, product.price, product.title, product.size, product.color) }} className="flex ml-4 text-white bg-[#007fff] border-0 py-2 px-3 md:px-6 focus:outline-none hover:bg-indigo-600 rounded">Buy Now</button>
+                                {product.availableQty > 0 && <span className="title-font font-medium text-2xl text-gray-900">₹58.00</span>}
+                                {product.availableQty <= 0 && <span className="title-font font-medium text-2xl text-gray-900">Out of stock!</span>}
+                                <button disabled={product.availableQty <= 0} onClick={() => { addToCart(slug, 1, product.price, product.title, product.size, product.color) }} className="flex ml-8 text-white bg-[#007fff] border-0 py-2 px-3 md:px-6 focus:outline-none hover:bg-indigo-600 rounded disabled:bg-blue-400">Add to Cart</button>
+                                <button disabled={product.availableQty <= 0} onClick={() => { buyNow(slug, 1, product.price, product.title, product.size, product.color) }} className="flex ml-4 text-white bg-[#007fff] border-0 py-2 px-3 md:px-6 focus:outline-none hover:bg-indigo-600 rounded disabled:bg-blue-400">Buy Now</button>
                                 {/* <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                                     <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                                         <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
