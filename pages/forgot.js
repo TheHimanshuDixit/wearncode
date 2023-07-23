37c8321d1fb2a1ac880a8b0bd564d75a95cf5066
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-07-23 20:17:01 +0530

    bug fixes

--- pages/forgot.js
+++ pages/forgot.js
@@ -4,6 +4,7 @@ import router from 'next/router'
 import React, { useEffect, useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Head from 'next/head';
 
 const Login = () => {
 
