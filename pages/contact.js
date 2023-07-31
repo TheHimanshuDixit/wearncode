413d0d6927de5120d481c73f6920a7a450aacf98
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-08-01 00:44:13 +0530

    contact us page added

--- pages/contact.js
+++ pages/contact.js
@@ -1,13 +1,121 @@
-import Head from 'next/head'
-import React from 'react'
+import Head from 'next/head';
+import React, { useState } from 'react';
 
 const Contact = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [message, setMessage] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const data = { phone, name, email, message };
+
+    fetch(`/api/postcontact`, {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data);
+        alert("Thanks for contacting us");
+        setPhone('')
+        setName('')
+        setMessage('')
+        setEmail('')
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+
+  }
+  const handleChange = (e) => {
+    if (e.target.name == 'phone') {
+      setPhone(e.target.value)
+    }
+    else if (e.target.name == 'email') {
+      setEmail(e.target.value)
+    }
+    else if (e.target.name == 'message') {
+      setMessage(e.target.value)
+    }
+    else if (e.target.name == 'name') {
+      setName(e.target.value)
+    }
+  }
   return (
     <>
       <Head>
         <title>Contact Us</title>
       </Head>
-      <h1>Contact Us</h1>
+      <section className="text-gray-600 body-font relative">
+        <div className="container px-5 py-12 mx-auto">
+          <div className="flex flex-col text-center w-full mb-12">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, natus!</p>
+          </div>
+          <div className="lg:w-1/2 md:w-2/3 mx-auto">
+            <form onSubmit={handleSubmit} className="flex flex-wrap -m-2">
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label htmlFor="name" className="leading-7 text-sm text-gray-600">Enter your name</label>
+                  <input value={name} onChange={handleChange} type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-pink-500 focus:bg-white focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label htmlFor="phone" className="leading-7 text-sm text-gray-600">Enter your phone</label>
+                  <input value={phone} onChange={handleChange} type="number" id="phone" name="phone" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-pink-500 focus:bg-white focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <div className="relative">
+                  <label htmlFor="email" className="leading-7 text-sm text-gray-600">Enter your email</label>
+                  <input value={email} onChange={handleChange} type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-pink-500 focus:bg-white focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <div className="relative">
+                  <label htmlFor="message" className="leading-7 text-sm text-gray-600">Elaborate your concern</label>
+                  <textarea value={message} onChange={handleChange} name='message' id="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-pink-500 focus:bg-white focus:ring-2 focus:ring-pink-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" />
+                </div>
+              </div>
+              <div className="p-2 w-full">
+                <button type='submit' className="flex mx-auto text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-600 rounded text-lg">Submit</button>
+              </div>
+              <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center flex justify-center flex-col">
+                <a className="text-pink-500 mb-2">blog@email.com</a>
+                <span className="inline-flex justify-center">
+                  <a className="text-gray-500">
+                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                    </svg>
+                  </a>
+                  <a className="ml-4 text-gray-500">
+                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                    </svg>
+                  </a>
+                  <a className="ml-4 text-gray-500">
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                      <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                      <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                    </svg>
+                  </a>
+                  <a className="ml-4 text-gray-500">
+                    <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                    </svg>
+                  </a>
+                </span>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
